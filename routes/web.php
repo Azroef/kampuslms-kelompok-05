6b65e295b092f71671a11e2967a5d1290d87445a
Muchammad Maulana <maulana@example.com>
2026-09-09 23:06:28 +0800

    Refactor: Fix duplikat routes dan standardize parameter naming
    - Hapus duplikat route /mata-kuliah/{mataKuliah} dan /mata-kuliah/{id}
    - Standarisasi parameter menggunakan {id}
    - Semua routes sudah konsisten menggunakan route() helper

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,6 @@ Route::get('/mata-kuliah', [CourseController::class, 'index'])
     ->name('mata-kuliah.index'); 
  
 // Route untuk menampilkan detail satu mata kuliah. 
-Route::get('/mata-kuliah/{mataKuliah}', [CourseController::class, 'show']) 
-    ->name('mata-kuliah.show'); 
-     
 Route::get('/mata-kuliah/{id}', [CourseController::class, 'show']) 
     ->name('mata-kuliah.show'); 
  
